2c32b495ef406baf497b3b26b6fec2667ef72163
Jeremy <jeremy24wu@gmail.com>
2026-05-29 00:27:53 +1000

    fix pathing for pip install pywin32

--- main.py
+++ main.py
@@ -2,22 +2,18 @@ import sys
 from pathlib import Path
 
 root = Path(__file__).parent
+lib_dir = root / "lib"
+lib_dir.mkdir(parents=True, exist_ok=True)
+
 sys.path.insert(0, root.as_posix())
-sys.path.extend(
-    path.as_posix()
-    for path in (
-        root / "lib",
-        root / "lib" / "win32",
-        root / "lib" / "win32" / "lib",
-    )
-)
+sys.path.insert(0, lib_dir.as_posix())
 
 from flogin.utils import setup_logging
 from flogin import Pip
 
 setup_logging()
 
-with Pip() as pip:
+with Pip(libs_dir=lib_dir) as pip:
     pip.ensure_installed("pywin32", module="pywintypes")
 
 from OutlookAgendaPlugin.plugin import OutlookAgendaPlugin
